0510dd1fcd5d40a16f7037d3321ff1ea20809768
Jagdish Pathakji <jagdishgithub1@gmail.com>
2026-07-23 17:38:44 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = "https://raegan-asynchronous-uriel.ngrok-free.dev";
+// export const API_BASE_URL = "https://raegan-asynchronous-uriel.ngrok-free.dev";
+export const API_BASE_URL = "https://modified-babied-shredder.ngrok-free.dev";
 
 export const COMMON_HEADERS = {
     "Content-Type": "application/json",
